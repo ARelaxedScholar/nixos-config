8ffa3042ff1a3e18a5cc96069712583397cc61d9
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2025-10-20 13:56:08 +0000

    feat: Added beginning of a flake.nix and fixed a logic error that I think, was causing a recursion loop during installation

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
-#/root/nixos-config/flake.nix
-
 {
-	description="My NixOS System Flake (cuz I'm a baller)"
+  description = "ARelaxedScholar's NixOS System Flake";
+  
+  inputs = {
+ 	
+  	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	disko.url = "github:nix-community/dislo";
+	home-manager.url = "github:nix-community/home-manager";
+	impermanence.url = "github:nix-community/impermanence";
+  };
 }
-
-inputs = {
-	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	disko.url = "github:nix-community/disko";
-};
