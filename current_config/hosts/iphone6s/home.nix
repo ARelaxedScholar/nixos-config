8ee0d14c31c82c2a914f335555467d58b7b914cd
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2025-12-07 20:30:49 -0500

    Remove unsupported Niri target and use hardcoded wallpaper path

--- current_config/hosts/iphone6s/home.nix
+++ current_config/hosts/iphone6s/home.nix
@@ -73,7 +73,6 @@ in
     enable = true;
     image = if selectedWallpaper != "" then ../../wallpapers/${selectedWallpaper} else null;
     polarity = "dark";
-    targets.niri.enable = true;
   };
 
   home.stateVersion = "25.05";
